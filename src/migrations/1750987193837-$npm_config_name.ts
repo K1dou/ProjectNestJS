9c33243f008643a8ee5b7a6a4edef64825c8100c
K1dou <hique1276@gmail.com>
2025-06-26 22:24:00 -0300

    feat: add TypeORM configuration and migration scripts for user entity

---
+++ src/migrations/1750987193837-$npm_config_name.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class  $npmConfigName1750987193837 implements MigrationInterface {
+    name = ' $npmConfigName1750987193837'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "phone" character varying, "cpf" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
